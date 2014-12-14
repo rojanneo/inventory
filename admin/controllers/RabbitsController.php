271eec3fdd836f2b2533e00f34cd1383ebcb2999
rojanneo <rojan_neo@hotmail.com>
2014-12-14 15:05:47 +0545

    Dec 14

--- admin/controllers/RabbitsController.php
+++ admin/controllers/RabbitsController.php
@@ -8,21 +8,8 @@ class RabbitsController extends Controller
 
 	public function indexAction()
 	{
-		$families = getModel('family')->getCollection();
-		foreach($families as $family)
-		{
-			echo '<h3>Family '.$family.'</h3>';
-			$rabbits = (getModel('rabbit')->getFamilyRabbits($family));
-			foreach($rabbits as $rabbit_id)
-			{
-				$rabbit = getModel('rabbit')->load($rabbit_id);
-				echo $rabbit['product_name'].' ';
-				$dob = new DateTime($rabbit['rabbit_dob']);
-				$today = new DateTime(date('Y-m-d'));
-				$diff = $today->diff($dob)->format("%a");
-				echo $diff.' days';
-				if($diff >= 4) echo 'Ready to Mate';
-			}
-		}
+		$data['families'] = getModel('family')->getCollection();
+
+		$this->view->renderAdmin('rabbits/list.phtml',$data);
 	}
 }
